11488b73cdd8d83aa5536fe7a218ae4c28f8e123
Sonu Kumar <60311092+sonu2614@users.noreply.github.com>
2023-07-13 15:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function generatePromise(){
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve("Hello world!")
+		},1000)
+	})
+}
+
+generatePromise().then((data) => {
+	const element=document.getElementById("output")
+	element.innerText=data;
+})
